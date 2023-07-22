485e7f22605f5d1a21a48316396f8eae35a3a76f
aviralrabbit1 <aviral.robin@gmail.com>
2023-07-23 00:22:57 +0530

    web-server done

--- web-server/Readme.md
+++ web-server/Readme.md
@@ -12,9 +12,36 @@
 | BODY   | { text: "hello", photo: "smile.jpg" } |
 | HEADERS | Host: facebook.com                    |
 
-#### HEADERS:-
+### HEADERS:-
 
 | HEADERS      | Content-Type: application/json        |
 |-------------|---------------------------------------|
 | BODY        | { text: "hello", photo: "smile.jpg" } |
 | STATUS CODE | 200 (successful)                      |
+
+
+For *`https://www.google.com:443/maps`*, **Origin** is combination of
+- Protocol: **https://**
+- Host(server to be browsed): **www.google.com**
+- Port(included in request): **www.google.com:443/maps**
+
+
+### **`CORS`** - Cross Origin Resource Sharing
+
+**Access-Control-Allow-Origin:**
+- *(for allowing all)
+- **Whitelisting** => Explicitily specific allowance(Eg. https://www.google.com)
+
+
+### check methods
+we can check the request **POST** method by writing in broswer console, recieved in our running server-terminal, that the data is logged correctly
+```
+fetch('http://localhost:3000/friends',{
+    method: 'POST',
+    body: JSON.stringify({id: 5, name: "Aman Katara"})
+})
+.then((response) => response.json())
+.then((friend) => console.log(friend))
+```
+
+
--- web-server/index.js
+++ web-server/index.js
@@ -2,6 +2,29 @@ const http = require('http');
 
 const PORT = 3000;
 
+const friends = [
+    {
+        id: 0,
+        name: 'Arijit Sahai'
+    },
+    {
+        id: 1,
+        name: 'Ujjwal Dhir'
+    },
+    {
+        id: 2,
+        name: 'Naveen Kumar'
+    },
+    {
+        id: 3,
+        name: 'Divyanand'
+    },
+    {
+        id: 4,
+        name: 'Pranjal Agarwal'
+    }
+]
+
 // const server = http.createServer((req, res) => { //request listener
 //     // req - header and data passsed from client
 //     // res - create response writign some data and headers send back to client
@@ -20,18 +43,35 @@ const PORT = 3000;
 const server = http.createServer();
 
 server.on('request', (req, res) => { 
-    if(req.url === '/friends'){
+
+    const items = req.url.split('/'); //splitting a url
+
+    if(req.method === 'POST' && items[1] === 'friends'){
+        // data passing as stream from the url, recieved as buffer
+        req.on('data', (data) => {
+            const friend = data.toString(); //converted to string to print
+            console.log('Request: ', friend); 
+            friends.push(JSON.parse(friend)); //converted to json to push into array
+            // req.pipe(res); will pipe incomplete response here
+        });
+        req.pipe(res); // pipe the req listener after the data event
+        // res.end() isn't required when req ends when we pipe
+    } 
+    else if(req.method === 'GET' && items[1] === 'friends'){ // req.url = /items[1]
         // res.writeHead(200, {
         //     'Content-Type': 'application/json',
         // });
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({
-            name: "Aviral Verma", 
-            messages: 'Hello Aviral, how are you doing?',
-        }));
+        if(items.length === 3){
+            const friendIndex = Number(items[2])
+            res.end(JSON.stringify(friends[friendIndex]));            
+        }
+        else {
+            res.end(JSON.stringify(friends));
+        }
     }
-    else if(req.url === '/messages'){
+    else if(req.method === 'GET' && items[1] === 'messages'){
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<body>');
