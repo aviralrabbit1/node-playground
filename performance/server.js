26d7fa66a037b87fdcabbf75ecac535326a78994
Aviral Verma <aviral.robin@gmail.com>
2023-09-20 14:53:22 +0530

    blocking example commments

--- performance/server.js
+++ performance/server.js
@@ -3,21 +3,25 @@ const express = require('express');
 const app = express();
 
 // Naive approach, the amount of time it'll wait
-function delay(duration) {
+function delay(duration) { // simulating extreme worstcase of blocking behaviour
     const startTime = Date.now();    
     while (Date.now() - startTime < duration) {
         // event loop is blocked        
     }
 }
+// ran two instances of /timer, the second one started after the first one ended after 9 seconds
 
 app.get('/', (req, res) => {
-    res.send('Performance example')
+    // JSON.stringify({}) => "{}" // Blocking function
+    // JSON.parse("{}") => {}
+    res.send('Performance example');
 });
 
 app.get('/timer', (req, res) => {
     // delay the response
     delay(9000);
-    res.send('Delayed')
+    res.send('Delayed');
 })
 
 app.listen(3000);
+
